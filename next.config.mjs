68de046b9dec5a44a592e99fef45e31c979986c2
nhanifah <119664502+nhanifah@users.noreply.github.com>
2025-05-25 19:03:50 +0700

    fix: path

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
+const isProd = true
 
 const nextConfig = {
   output: 'export',
   basePath: '',
   assetPrefix: isProd ? '' : '',
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
